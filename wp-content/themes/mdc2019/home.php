de8b470ef709115614d376d2d657f4528fceb04e
imeuro <mauro.fioravanzi@gmail.com>
2019-11-13 18:36:08 +0100

    db local wip

---
+++ wp-content/themes/mdc2019/home.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}	
+get_header(); ?>
+
+	<div id="primary" <?php generate_do_element_classes( 'content' ); ?>>
+		<main id="main" <?php generate_do_element_classes( 'main' ); ?>>
+			<?php
+			/**
+			 * generate_before_main_content hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_before_main_content' );
+
+			?>
+			
+
+
+			<section id="latestNews" class="section section-home section-home-latestnews">
+				<div class="inside-article">
+		
+					<header class="section-header">
+						<h2 class="section-title" itemprop="headline"><a href="https://localhost/maialidacorsa2019/2019/10/30/mini-climbing/" rel="bookmark">ULTIME NEWS</a></h2>			
+					</header><!-- .section-header -->
+		
+					<div class="section-summary" itemprop="text">
+						
+
+					</div><!-- .section-summary -->
+
+					<footer class="section-meta"></footer><!-- .section-meta -->
+				</div><!-- .inside-article -->
+			</section>
+
+			<section id="latestPhotos" class="section section-home section-home-latestphotos">
+				<div class="inside-article">
+		
+					<header class="section-header">
+						<h2 class="section-title" itemprop="headline"><a href="https://localhost/maialidacorsa2019/2019/10/30/mini-climbing/" rel="bookmark">ULTIME FOTOGRAFIE</a></h2>			
+					</header><!-- .section-header -->
+		
+					<div class="section-summary" itemprop="text">
+						
+
+					</div><!-- .section-summary -->
+
+					<footer class="section-meta"></footer><!-- .section-meta -->
+				</div><!-- .inside-article -->
+			</section>
+
+
+			<section id="infoUtili" class="section section-home section-home-infoutili">
+				<div class="inside-article">
+		
+					<header class="section-header">
+						<h2 class="section-title" itemprop="headline">INFO UTILI</h2>
+					</header><!-- .section-header -->
+		
+					<div class="section-summary" itemprop="text">
+						
+
+					</div><!-- .section-summary -->
+
+					<footer class="section-meta"></footer><!-- .section-meta -->
+				</div><!-- .inside-article -->
+			</section>
+
+
+
+			<?php
+
+			/**
+			 * generate_after_main_content hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_after_main_content' );
+			?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	<?php
+	/**
+	 * generate_after_primary_content_area hook.
+	 *
+	 * @since 2.0
+	 */
+	do_action( 'generate_after_primary_content_area' );
+
+	generate_construct_sidebars();
+
+get_footer();
