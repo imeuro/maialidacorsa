6a28a03b59a8542c5a37d192598f31e911722560
imeuro <mauro.fioravanzi@gmail.com>
2020-01-21 14:14:50 +0100

    analytics! xD

--- wp-content/themes/mdc2020/footer.php
+++ wp-content/themes/mdc2020/footer.php
@@ -61,5 +61,15 @@ do_action( 'generate_after_footer' );
 wp_footer();
 ?>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156557811-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-156557811-1');
+</script>
+
 </body>
 </html>
