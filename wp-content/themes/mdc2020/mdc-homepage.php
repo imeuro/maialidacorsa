fa9f66b76f9a95854b8259ba887d99ac5645e5fc
imeuro <mauro.fioravanzi@gmail.com>
2020-02-04 15:44:25 +0100

    better excerpt

--- wp-content/themes/mdc2020/mdc-homepage.php
+++ wp-content/themes/mdc2020/mdc-homepage.php
@@ -32,10 +32,11 @@ get_header(); ?>
 					</header><!-- .section-header -->
 		
 					<div class="section-summary" itemprop="text">
-						<a href="<?php echo $about->guid; ?>" title="<?php echo $about->post_title; ?>">
+						<a href="<?php echo get_permalink($about->id); ?>" title="<?php echo $about->post_title; ?>">
 							<img width="300" height="228" src="https://www.maialidacorsa.it/wp-content/uploads/2020/02/100-minista-1-300x228.png" class="attachment-medium size-medium" alt="100% MINISTA - Maiali Da Corsa" loading="lazy" srcset="https://www.maialidacorsa.it/wp-content/uploads/2020/02/100-minista-1-300x228.png 300w, https://www.maialidacorsa.it/wp-content/uploads/2020/02/100-minista-1.png 595w" sizes="(max-width: 300px) 100vw, 300px">
-							<p><?php echo $about->post_excerpt; ?></p>
 						</a>
+						<p><?php echo get_the_excerpt($about->id;) ?></p>
+						
 					</div><!-- .section-summary -->
 
 					<footer class="section-meta"></footer><!-- .section-meta -->
